cd4f40446e18b2631fc0d5f96b58c5ce8731672a
Net User <mab2zq@virginia.edu>
2026-05-22 10:59:51 +0200

    revised generation

--- run_train.sh
+++ run_train.sh
@@ -5,7 +5,7 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_109_cuda/x86_64-el9-gcc13-opt/setup.sh
 export PYTHONPATH="/eos/user/m/mbaessle/.local/lib/python3.13/site-packages:${PYTHONPATH:-}"
 
 # Change directory
-cd /eos/user/m/mbaessle/flow_trainer
+cd ./
 
 # Run training
 python3 train_flow.py
